bf8cf87deb56175167595f9a756af54e7e274cba
adjuchas <1312804080@qq.com>
2022-05-09 02:31:09 +0800

    22-05-09

--- 98.py
+++ 98.py
@@ -1,2 +1,14 @@
+from typing import List
 
 
+class Solution:
+    def diStringMatch(self, s: str) -> List[int]:
+        lis = [n for n in range(0, len(s))]
+        ans = []
+        for i in s:
+            if i == 'I':
+                ans.append(lis.pop(0))
+            else:
+                ans.append(lis.pop())
+        ans.append(lis[0])
+        return ans
